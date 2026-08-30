019125900bf009a963cfcd6a6240c0642772c81e
thluongm00803-code <thluongm00803@gmail.com>
2026-08-30 22:48:20 +0700

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLLc80m6llnGa-EH2V9yiN9e86y5YITMo",
-  authDomain: "quan-li-giam-thi-hs.firebaseapp.com",
-  projectId: "quan-li-giam-thi-hs",
-  storageBucket: "quan-li-giam-thi-hs.firebasestorage.app",
-  messagingSenderId: "639742326349",
-  appId: "1:639742326349:web:04aa99eeb6be7526d04e04",
-  measurementId: "G-J69DH82C8H"
+  apiKey: "AIzaSyDHEV9eeSKMjQ7hoRV9WSrugKUfelJO5io",
+  authDomain: "quan-li-hs.firebaseapp.com",
+  projectId: "quan-li-hs",
+  storageBucket: "quan-li-hs.firebasestorage.app",
+  messagingSenderId: "904619592458",
+  appId: "1:904619592458:web:cdb1b891e74553b1ba88f3",
+  measurementId: "G-HN69D7RYHT"
 };
 
 const app = initializeApp(firebaseConfig);
